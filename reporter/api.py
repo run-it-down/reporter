8093cb978b46235a3b55632b79f1eb197ded0667
zkdev <dev@zeekay.dev>
2021-04-09 12:53:23 +0200

    Add common games endpoint

--- reporter/api.py
+++ reporter/api.py
@@ -24,40 +24,53 @@ class Main:
         }
 
         # get WR
-        logger.info('getting WR')
+        logger.info('getting wr')
         wr = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/winrate'), params=params)
 
         # get KDA
-        logger.info('getting KDA')
+        logger.info('getting kda')
         kda = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/kda'), params=params)
 
         # get CS
-        logger.info('getting CS')
+        logger.info('getting cs')
         cs = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/cs'), params=params)
 
         # get avg-game
+        logger.info('getting average game')
         avg_game = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/avg-game'), params=params)
 
         # millionaire
+        logger.info('getting classification millionaire')
         millionaire = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/avg-game'), params=params)
 
         # match-type
+        logger.info('getting classification match-type')
         match_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/match-type'), params=params)
 
         # murderous-duo
+        logger.info('getting classification murderous-duo')
         murderous_duo = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/murderous-duo'), params=params)
 
         # duo-type
+        logger.info('getting classification duo-type')
         duo_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/duo-type'), params=params)
 
         # farmer-type
+        logger.info('getting classification farmer-type')
         farmer_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/farmer-type'), params=params)
 
         # tactician
+        logger.info('getting classification tactician')
         tactician = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/tactician'), params=params)
+
+        # common_games
+        logger.info('getting common games')
+        common_games = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/common_games'), params=params)
+
 
         # aggregate metrics to report
         resp.body = json.dumps({
+            'games_analyzed': json.loads(common_games),
             'winrate': json.loads(wr.content),
             'kda': json.loads(kda.content),
             'cs': json.loads(cs.content),
