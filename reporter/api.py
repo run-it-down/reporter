c947a56484891a86a098b62c2407660f14c6648d
zkdev <dev@zeekay.dev>
2021-04-09 09:54:09 +0200

    Add classification to report

--- reporter/api.py
+++ reporter/api.py
@@ -18,37 +18,56 @@ class Main:
 
     def on_get(self, req, resp):
         logger.info('reporting')
-
-        # get WR
         params = {
             'summoner1': req.params['summoner1'],
             'summoner2': req.params['summoner2']
         }
+
+        # get WR
+        logger.info('getting WR')
         wr = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/winrate'), params=params)
 
         # get KDA
-        params = {
-            'summoner1': req.params['summoner1'],
-            'summoner2': req.params['summoner2']
-        }
+        logger.info('getting KDA')
         kda = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/kda'), params=params)
 
         # get CS
-        params = {
-            'summoner1': req.params['summoner1'],
-            'summoner2': req.params['summoner2']
-        }
+        logger.info('getting CS')
         cs = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/cs'), params=params)
 
-        logger.info(wr.content)
-        logger.info(kda.content)
-        logger.info(cs.content)
+        # get avg-game
+        avg_game = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/avg-game'), params=params)
+
+        # millionaire
+        millionaire = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/avg-game'), params=params)
+
+        # match-type
+        match_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/match-type'), params=params)
+
+        # murderous-duo
+        murderous_duo = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/murderous-duo'), params=params)
+
+        # duo-type
+        duo_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/duo-type'), params=params)
+
+        # farmer-type
+        farmer_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/farmer-type'), params=params)
+
+        # tactician
+        tactician = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/tactician'), params=params)
 
         # aggregate metrics to report
         resp.body = json.dumps({
             'winrate': json.loads(wr.content),
             'kda': json.loads(kda.content),
             'cs': json.loads(cs.content),
+            'avg-game': json.loads(avg_game.content),
+            'classification_millionaire':  json.loads(millionaire.content),
+            'match_type': json.loads(match_type),
+            'murderous_duo': json.loads(murderous_duo),
+            'duo_type': json.loads(duo_type),
+            'farmer_type': json.loads(farmer_type),
+            'tactician': json.loads(tactician),
         })
 
 
