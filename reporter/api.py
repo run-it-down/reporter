9c638f5fbe5e2b88444bbc25cdddbdbaa2cee90a
zkdev <dev@zeekay.dev>
2021-04-09 13:58:27 +0200

    Properly load reponse content

--- reporter/api.py
+++ reporter/api.py
@@ -45,42 +45,41 @@ class Main:
 
         # match-type
         logger.info('getting classification match-type')
-        match_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/match-type'), params=params)
+        match_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/classification/match-type'), params=params)
 
         # murderous-duo
         logger.info('getting classification murderous-duo')
-        murderous_duo = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/murderous-duo'), params=params)
+        murderous_duo = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/classification/murderous-duo'), params=params)
 
         # duo-type
         logger.info('getting classification duo-type')
-        duo_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/duo-type'), params=params)
+        duo_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/classification/duo-type'), params=params)
 
         # farmer-type
         logger.info('getting classification farmer-type')
-        farmer_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/farmer-type'), params=params)
+        farmer_type = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/classification/farmer-type'), params=params)
 
         # tactician
         logger.info('getting classification tactician')
-        tactician = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/tactician'), params=params)
+        tactician = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/classification/tactician'), params=params)
 
         # common_games
         logger.info('getting common games')
-        common_games = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/common_games'), params=params)
-
+        common_games = requests.get(url=util.urljoin(ANALYZER_ENDPOINT, '/common-games'), params=params)
 
         # aggregate metrics to report
         resp.body = json.dumps({
-            'games_analyzed': json.loads(common_games.content),
+            'games_analyzed': json.loads(common_games.content.decode())['common_games'],
             'winrate': json.loads(wr.content),
             'kda': json.loads(kda.content),
             'cs': json.loads(cs.content),
             'avg-game': json.loads(avg_game.content),
             'classification_millionaire':  json.loads(millionaire.content),
-            'match_type': json.loads(match_type),
-            'murderous_duo': json.loads(murderous_duo),
-            'duo_type': json.loads(duo_type),
-            'farmer_type': json.loads(farmer_type),
-            'tactician': json.loads(tactician),
+            'match_type': json.loads(match_type.content),
+            'murderous_duo': json.loads(murderous_duo.content),
+            'duo_type': json.loads(duo_type.content),
+            'farmer_type': json.loads(farmer_type.content),
+            'tactician': json.loads(tactician.content),
         })
 
 
